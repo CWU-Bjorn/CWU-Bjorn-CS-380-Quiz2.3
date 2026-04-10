c90c665d06aec76cf7b9ab01558e668d5ba8610e
CWU-Bjorn <AddingtonB@cwu.edu>
2026-04-09 21:46:13 -0700

    Update QuizFor.java

--- QuizFor.java
+++ QuizFor.java
@@ -6,7 +6,7 @@ public class QuizFor {
 		
 	System.out.println("Hello World");
 	
-	System.out.println("Never forget for the beauty of the world is not always the garden but also the gardern wall");
+	System.out.println("Never forget for the beauty of the world is not always the garden but also the garden wall");
 	}
 	
 }
