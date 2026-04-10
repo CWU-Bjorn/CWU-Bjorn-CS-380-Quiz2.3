89cc28a30b5ff160dd625abafc9f4b1d2fc360c1
CWU-Bjorn <AddingtonB@cwu.edu>
2026-04-09 21:40:46 -0700

    Second commit of code

--- QuizFor.java
+++ QuizFor.java
@@ -6,6 +6,7 @@ public class QuizFor {
 		
 	System.out.println("Hello World");
 	
+	System.out.println("Never forget for the beauty of the world is not always the garden but also the gardern wall");
 	}
 	
 }
